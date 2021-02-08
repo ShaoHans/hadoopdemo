5c48152087c277657f53b6eb150082a6068b69d5
邵汉中 <635260101@qq.com>
2021-02-08 20:46:33 +0800

    init

---
+++ src/main/java/hdfs/GenerateFile.java
@@ -0,0 +1,23 @@
+package hdfs;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.util.Random;
+
+public class GenerateFile {
+    public static void main(String[] args) throws Exception {
+        /**
+         * 生成文本文件，每行文本内容格式为：{i} Hello {language}，其中i为循环变量值，language的值从数组中随机获取一个
+         */
+        FileOutputStream fos = new FileOutputStream("test.txt");
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        String[] languages = new String[]{"c#", "java", "python", "go", "c", "c++"};
+        Random random = new Random();
+        for (int i = 1; i <= 100000; i++) {
+            String str = i + " Hello " + languages[random.nextInt(languages.length)] + System.lineSeparator();
+            bos.write(str.getBytes());
+        }
+        bos.flush();
+    }
+}
+
---
+++ src/main/java/hdfs/TestUpload.java
@@ -0,0 +1,38 @@
+package hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+
+
+public class TestUpload {
+    static Configuration conf;
+    static FileSystem fs;
+
+    public static void main(String[] args) throws Exception {
+        conf = new Configuration();
+        // 设置块大小为1M
+        conf.set("dfs.block.size", "1048576");
+        fs = FileSystem.get(URI.create("hdfs://mycluster/"), conf);
+        BufferedInputStream fis = new BufferedInputStream(new FileInputStream("test.txt"));
+        Path out = getFilePath("/hadoop/data.txt");
+        FSDataOutputStream fsDataOutputStream = fs.create(out);
+        IOUtils.copyBytes(fis, fsDataOutputStream, conf, true);
+    }
+
+    public static Path getFilePath(String filePath) throws IOException {
+        Path path = new Path(filePath);
+        if (fs.exists(path)) {
+            fs.delete(path, true);
+        }
+        return path;
+    }
+}
+
